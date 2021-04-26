2c2ddcbbd5cf7cdf9f85c746473f232f8691b9cf
HANG YANG <hyan0058@student.monash.edu>
2021-04-26 21:44:59 +1000

    add the practice file to git

---
+++ model/demo.java
@@ -0,0 +1,19 @@
+
+public class Demo{
+    private String name;
+    private String course;
+
+    public Demo(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+}
