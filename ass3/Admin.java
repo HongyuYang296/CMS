05d390258dcbdd1b43b80a1c8afe5084a6a7290f
YuChen Bai <ybai0021@student.monash.edu>
2021-05-13 15:17:55 +1000

    Admin class

--- ass3/Admin.java
+++ ass3/Admin.java
@@ -1,2 +1,14 @@
 public class Admin {
+    private int Adminid;
+    private  String AdminName;
+    private  String AdminEmail;
+    private  String AdminPassword;
+    private  String AdminType;
+
+
+
+
+
+
+
 }
